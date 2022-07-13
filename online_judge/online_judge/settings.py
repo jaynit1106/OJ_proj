907a8014850915616c43b90d0b7a4c1d94019a5b
jaynit1106 <jaynitpatel11062001@gmail.com>
2022-07-13 10:39:09 +0530

    minor bug

--- online_judge/online_judge/settings.py
+++ online_judge/online_judge/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-33=8c$gr^*!*78z*7f!p_^rng&l^le^$x$!6y3au@)d)77h-d3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','codelearner-iiit.herokuapp.com']
 
 
 # Application definition
